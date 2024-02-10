19e8fc6d852db1879bc3f1a9c259b5d2b7a1100a
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2024-02-10 21:58:28 +0530

    Popular hotel done

---
+++ src/app/components/PopularHotels.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PopularHotelCard from './PopularHotelCard'
+
+const PopularHotels = () => {
+  const hotels = [
+    {
+      img:"hotel-1.jpeg",
+      location:"London NW8 7JT England",
+      name:"Danubius Hotel Regents Park",
+      price:"$200 Par Night",
+      rate:4.8
+    },
+    {
+      img:"hotel-2.jpeg",
+      location:"London NW8 7JT England",
+      name:"The Resident Soho",
+      price:"$200 Par Night",
+      rate:4.8
+    },
+    {
+      img:"hotel-3.jpeg",
+      location:"London NW8 7JT England",
+      name:"London Bridge Hotel",
+      price:"$200 Par Night",
+      rate:4.8
+    }
+  ]
+  return (
+    <div className='mt-[5rem]'>
+      <h2 className='font-semibold text-[2.5rem]'>Our most popular Hotels</h2>
+      <div className='flex justify-between'>
+
+        <p className='text-[1rem]  text-[#555555]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <button className='bg-[#24AB701A] text-[#24AB70] border rounded-full w-[5rem] h-[2rem]'>View All</button>
+      </div>
+
+
+      <div className="main-grid flex gap-5 mt-[3rem]">
+      
+        {hotels.map((hotel,index)=>{
+          return(
+
+            <PopularHotelCard
+            key={index}
+            img={hotel.img}
+            location={hotel.location}
+            name={hotel.name}
+            price={hotel.price}
+            rate={hotel.rate}/>
+            )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default PopularHotels
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
-import Image from "next/image";
 import Service from "./components/Service";
 import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
+import ViewsHotels from "./components/PopularHotels";
 
 export default function Home() {
   return (
-    <div className="mx-[1.5rem]">
+    <div className="mx-[4rem]">
     <Navbar/>
     <Landing/>
+    <ViewsHotels/>
     <Service/>
     </div>
   )
